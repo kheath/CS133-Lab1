f5caac17856bcd53288a922e63940e28ccec413a
Melissa Galonsky <mgalonsky@cs.hmc.edu>
2016-02-05 13:57:25 -0800

    Wrote HeapPageId

--- src/java/simpledb/HeapPageId.java
+++ src/java/simpledb/HeapPageId.java
@@ -10,14 +10,16 @@ public class HeapPageId implements PageId {
      * @param tableId The table that is being referenced
      * @param pgNo The page number in that table.
      */
+    private int tabId;
+    private int pageNum;
     public HeapPageId(int tableId, int pgNo) {
-        // some code goes here
+        tabId = tableId;
+        pageNum = pgNo;
     }
 
     /** @return the table associated with this PageId */
     public int getTableId() {
-        // some code goes here
-        return 0;
+        return tabId;
     }
 
     /**
@@ -25,8 +27,7 @@ public class HeapPageId implements PageId {
      *   this PageId
      */
     public int pageNumber() {
-        // some code goes here
-        return 0;
+        return pageNum;
     }
 
     /**
@@ -36,8 +37,8 @@ public class HeapPageId implements PageId {
      * @see BufferPool
      */
     public int hashCode() {
-        // some code goes here
-        throw new UnsupportedOperationException("implement this");
+        int lengthOfPgNm = Math.floor(Math.log10(pageNum) + 1);
+        return pageNum + Math.pow(10, lengthOfPgNm) * tabId;
     }
 
     /**
@@ -48,8 +49,7 @@ public class HeapPageId implements PageId {
      *   ids are the same)
      */
     public boolean equals(Object o) {
-        // some code goes here
-        return false;
+        return ( pageNum == o.pageNum && tabId == o.tabId);
     }
 
     /**
