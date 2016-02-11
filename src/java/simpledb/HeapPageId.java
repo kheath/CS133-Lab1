30aac362b3a085add36485de426048e1d0d1a108
Kevin Heath <kheath@g.hmc.edu>
2016-02-10 22:31:45 -0800

    debugging HeapPageId

--- src/java/simpledb/HeapPageId.java
+++ src/java/simpledb/HeapPageId.java
@@ -50,7 +50,8 @@ public class HeapPageId implements PageId {
      */
     public boolean equals(Object o) {
     	HeapPageId p = (HeapPageId) o;
-        return ( pageNum == p.pageNumber() && tabId == p.getTableId());
+        return hashCode() == p.hashCode();
+    	//return ( pageNum == p.pageNumber() && tabId == p.getTableId());
     }
 
     /**
