81fbab7f54e76728ffb7ae603d324b1664d224d2
Kevin Heath <kheath@g.hmc.edu>
2016-02-10 22:12:11 -0800

    debugging HeapPage

--- src/java/simpledb/HeapPage.java
+++ src/java/simpledb/HeapPage.java
@@ -278,11 +278,13 @@ public class HeapPage implements Page {
         
     	int count = 0;
     	for(int j = 0; j<this.getHeaderSize(); j++) {
+    		System.out.println(header[j]);
     		for(int i = 0; i<8; i++) {
     			byte mask = (byte) Math.pow(2, i);
     			count += (this.header[j] & mask)/(Math.pow(2, i));
     			System.out.println(count);
     		}
+    		
     	}
     	System.out.println(numSlots);
         return numSlots-count;
